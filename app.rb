54ce777ef14d2d0a9f3c404b62be74340f0cd3ab
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-12 17:54:31 +0000

    getting started

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+giant_bomb_key = ENV.fetch("GIANT_BOMB_KEY")
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  @user_year = null
+  giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&filter=expected_release_year:#{@user_year}"
+
+  erb(:main)
+
 end
