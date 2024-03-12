f17a85df6d20c701b13663df808fa7fa25f3ad31
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-12 19:09:33 +0000

    basic functionality working

--- app.rb
+++ app.rb
@@ -7,9 +7,23 @@ giant_bomb_key = ENV.fetch("GIANT_BOMB_KEY")
 
 get("/") do
   
-  @user_year = null
-  giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&filter=expected_release_year:#{@user_year}"
-
   erb(:main)
+
+end
+
+get("/search") do
+
+  @game_list = []
+  user_year = params.fetch("year")
+  giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&field_list=name,deck&filter=expected_release_year:#{user_year}"
+  raw_response = HTTP.get(giant_bomb_url)
+  @parsed_response = JSON.parse(raw_response)
+  @results = @parsed_response.fetch("results")
+  @results.each do |number|
+    @game_list.push(number)
+  end
+
+  
+  erb(:search)
 
 end
