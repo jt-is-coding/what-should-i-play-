06515221df3e288eb1ca45ed23ac3624b80270c3
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-12 21:18:06 +0000

    Random functionality working

--- app.rb
+++ app.rb
@@ -27,3 +27,20 @@ get("/search") do
   erb(:search)
 
 end
+
+get("/search/:year") do
+
+  @game_list = []
+  user_year = params.fetch("year")
+  giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&field_list=name,deck&filter=expected_release_year:#{user_year}"
+  raw_response = HTTP.get(giant_bomb_url)
+  @parsed_response = JSON.parse(raw_response)
+  @results = @parsed_response.fetch("results")
+  @results.each do |number|
+    @game_list.push(number)
+  end
+
+  
+  erb(:search)
+
+end
